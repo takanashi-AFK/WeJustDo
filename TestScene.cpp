ebda2614a44ea7f4ca873613d5dd173bd70c09ec
sugawara yuto <117239251+nattoSG09@users.noreply.github.com>
2023-08-15 06:08:07 +0900

    a

--- TestScene.cpp
+++ TestScene.cpp
@@ -12,13 +12,8 @@ void TestScene::Initialize()
 {
 	SolidObject* p1 = CreateSolidObject<TestObject>(this, "defaultModel.fbx");
 	SolidObject* p2 = CreateSolidObject<TestObject>(this, "defaultModel.fbx");
-
 	dynamic_cast<TestObject*>(p1)->IsMove(false);
 
-	/*
-	* 同じオブジェクトを二つ以上作成しようとすると死んじゃう...
-	* Instantiateではできていたことができなくなってる?
-	*/
 }
 
 //更新
