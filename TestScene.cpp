57e3ae1398c6cfd6ad61ee581da9c4fd7385d2ef
sugawara yuto <117239251+nattoSG09@users.noreply.github.com>
2023-08-18 02:03:14 +0900

    da

--- TestScene.cpp
+++ TestScene.cpp
@@ -13,6 +13,9 @@ void TestScene::Initialize()
 	SolidObject* p1 = CreateSolidObject<TestObject>(this, "defaultModel.fbx");
 	SolidObject* p2 = CreateSolidObject<TestObject>(this, "defaultModel.fbx");
 	dynamic_cast<TestObject*>(p1)->IsMove(false);
+	dynamic_cast<TestObject*>(p1)->SetPositionX(-2.0f);
+
+
 }
 
 //çXêV
