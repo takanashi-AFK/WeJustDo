b2b22017de28a76543d03b2170d1dd503a04d317
sugawara yuto <117239251+nattoSG09@users.noreply.github.com>
2023-08-19 22:16:39 +0900

    da

--- SkySphere.cpp
+++ SkySphere.cpp
@@ -1,11 +1,15 @@
 #include "SkySphere.h"
+#include "Engine/Model.h"
 
 SkySphere::SkySphere(GameObject* parent)
+	:GameObject(parent,"SkySpheere")
 {
 }
 
 void SkySphere::Initialize()
 {
+	ASSIGN(hModel_, Model::Load("Models/SkySphere/BackGround.fbx"));
+	assert(hModel_ <= 0);
 }
 
 void SkySphere::Update()
@@ -14,4 +18,5 @@ void SkySphere::Update()
 
 void SkySphere::Draw()
 {
+	Model::SetTransform(hModel_,)
 }
