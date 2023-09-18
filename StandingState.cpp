b70706a8da651296ac23670a69e3eb2f08566175
Nayet <n4shoya@icloud.com>
2023-09-19 02:57:04 +0900

    ???

--- StandingState.cpp
+++ StandingState.cpp
@@ -10,43 +10,55 @@
 //更新
 void StandingState::Update(Player* _p)
 {
+
+
 	//入力処理
 	HandleInput(_p);
 	//重力をリセット
-	_p->IsAddGravity(false);
-	XMFLOAT3 ppos;
-	ppos = _p->GetPosition();
+	
+	Transform* ppos;
+	ppos = _p->GetTransformAddress();
+	//入力処理
+
+
 	RayCastData downDataL; {
 		//当たっているか確認
-		downDataL.start = XMFLOAT3(ppos.x - (PLAYER_MODEL_SIZE.x / 2),
-			ppos.y,
-			ppos.z);
+		downDataL.start = ppos->position_;
+		downDataL.start.x = ppos->position_.x - (PLAYER_MODEL_SIZE.x / 2);
+		downDataL.start.y = ppos->position_.y + 0.01;
 		XMStoreFloat3(&downDataL.dir, XMVectorSet(0, -1, 0, 0));
 		Model::RayCast(_p->GetPlayerOnGround(), &downDataL);
 	}
-	RayCastData downDataR; {
-		//当たっているか確認
-		downDataR.start = XMFLOAT3(ppos.x + (PLAYER_MODEL_SIZE.x / 2),
-			ppos.y - (PLAYER_MODEL_SIZE.y / 2),
-			ppos.z);
-		XMStoreFloat3(&downDataL.dir, XMVectorSet(0, -1, 0, 0));
-		Model::RayCast(_p->GetPlayerOnGround(), &downDataR);
-	}
-	if (downDataL.dist < (PLAYER_MODEL_SIZE.y / 2) && downDataR.dist < (PLAYER_MODEL_SIZE.y / 2)) {
-
+	if (downDataL.dist <= 0.5f) {
 		//めり込み分、位置を戻す
-		XMVECTOR length = { 0,(PLAYER_MODEL_SIZE.y / 2) - ((downDataL.dist + downDataR.dist) / 2) - 0.25 ,0 };
-		XMFLOAT3 tmpPos = _p->GetPosition();
-		XMStoreFloat3(&tmpPos, XMLoadFloat3(&tmpPos) + length); _p->SetPosition(tmpPos);
+		XMVECTOR length = { 0,(PLAYER_MODEL_SIZE.y / 2) - (downDataL.dist+0.5f)  ,0 };
+		XMStoreFloat3(&ppos->position_, length);
+		_p->IsAddGravity(false);
 		_p->SetAcceleration(0);
 	}
+	RayCastData downDataR; {
+		//当たっているか確認
+		downDataR.start = ppos->position_;
+		downDataR.start.x = ppos->position_.x + (PLAYER_MODEL_SIZE.x / 2);
+		downDataR.start.y = ppos->position_.y + 0.01;
+		XMStoreFloat3(&downDataR.dir, XMVectorSet(0, -1, 0, 0));
+		Model::RayCast(_p->GetPlayerOnGround(), &downDataR);
+	}
+	if (downDataR.dist <= 0.5f) {
+		//めり込み分、位置を戻す
+		XMVECTOR length = { 0,(PLAYER_MODEL_SIZE.y / 2) - (downDataR.dist + 0.5) ,0 };
+		XMStoreFloat3(&ppos->position_, length);
+		_p->IsAddGravity(false);
+		_p->SetAcceleration(0);
+	}
+
 	{//State変化
 
 		//running
 		if (Input::IsKey(DIK_D) || Input::IsKey(DIK_A))
 			_p->GetState()->ChangeState(_p->GetState()->pRunning_, _p, true);
 		//dead
-		if (ppos.y <= -10 && ppos.y >= -13)
+		if (ppos->position_.y <= -10 && ppos->position_.y >= -13)
 			_p->GetState()->ChangeState(_p->GetState()->pDead_, _p, true);
 
 		if (Input::IsKeyDown(DIK_SPACE))
@@ -62,7 +74,6 @@ void StandingState::Enter(Player* _p)
 
 	//重力をリセット
 	_p->SetAcceleration(0);
-
 	//アニメーションをストップ
 	Model::SetAnimFrame(_p->GetModelHandle(), 0, 0, 1);
 
